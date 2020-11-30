5b20f2ebc9bbe30606317c50d416d0014c4f6f24
罗智 <754729961@qq.com>
2020-11-30 14:29:38 +0800

    第一次提交

---
+++ src/main/java/com/dexcoder/demo/quartz/JobSyncFactory.java
@@ -0,0 +1,42 @@
+package com.dexcoder.demo.quartz;
+
+import com.dexcoder.demo.service.ScheduleJobService;
+import org.quartz.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.dexcoder.demo.model.ScheduleJob;
+import com.dexcoder.demo.vo.ScheduleJobVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * 同步的任务工厂类
+ *
+ * Created by Luo on 12/19/14.
+ */
+@Component
+public class JobSyncFactory implements Job {
+
+    @Autowired
+    private ScheduleJobService scheduleJobService;
+
+    /* 日志对象 */
+    private static final Logger LOG = LoggerFactory.getLogger(JobSyncFactory.class);
+
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+
+        LOG.info("JobSyncFactory execute");
+
+        JobDataMap mergedJobDataMap = jobExecutionContext.getMergedJobDataMap();
+        ScheduleJob scheduleJob = (ScheduleJob) mergedJobDataMap.get(ScheduleJobVo.JOB_PARAM_KEY);
+
+        System.out.println("jobName:" + scheduleJob.getJobName() + "  " + scheduleJob);
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<%@ page contentType="text/html; charset=gbk"%>
+<html>
+<head>
+    <title>Dexcoder - 敏捷的程序员！ </title>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <a href="list-schedule-job">任务列表</a>
+</body>
+</html>
